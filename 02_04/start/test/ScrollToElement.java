2485bd90dac20b9c69e11a6c60cc69c997cb3cf8
Natty0609 <45440244+Natty0609@users.noreply.github.com>
2022-04-10 23:58:45 +0200

    Initial commit

---
+++ 02_04/start/test/ScrollToElement.java
@@ -0,0 +1,27 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+
+public class ScrollToElement {
+    public static void main(String[] args) throws InterruptedException {
+
+        System.setProperty("webdriver.chrome.driver", "/Users/User/Downloads/ChromeDriver/chromedriver.exe");
+
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("https://formy-project.herokuapp.com/scroll");
+
+        WebElement name = driver.findElement(By.id("name"));
+        Actions actions = new Actions(driver);
+        actions.moveToElement(name);
+        name.sendKeys("Tahova Nataliya");
+
+        WebElement date = driver.findElement(By.id("date"));
+        date.sendKeys("09/04/2022");
+        Thread.sleep(3000);
+
+        driver.quit();
+    }
+}
